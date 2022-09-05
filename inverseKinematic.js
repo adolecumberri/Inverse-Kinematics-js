98c3b9c98be509ed25dbc02cdbab85c5f3e6d5db
adolecumberri <adolecumberri@gmail.com>
2022-09-06 00:54:34 +0200

    first segment of the kinetics tail

--- inverseKinematic.js
+++ inverseKinematic.js
@@ -5,19 +5,19 @@ function inverseKinematic() {
     let canvas = document.getElementById("canvas"); //create canvas
     let ctx = canvas.getContext("2d");
 
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    let count = canvas.width >= canvas.height ? canvas.width * canvas.width : canvas.height * canvas.height;
-    canvas.style.backgroundColor = '#515151' 
+    let mouseX
+    let mouseY
+    
+    canvas.style.backgroundColor = '#515151'
     // ctx.fillStyle = '#ffffff';
 
-    class Segment{
+    class Segment {
         //a and b are points of vector.
-        a = {x: 0, y:0}; //Pvector
-        b = {x: 0, y:0}; //Pvector
+        a = { x: 0, y: 0 }; //Pvector
+        b = { x: 0, y: 0 }; //Pvector
         angle;
         len;
-        constructor({x,y,len,angle, ctx}) {
+        constructor({ x, y, len, angle, ctx }) {
             this.x = x
             this.y = y
 
@@ -37,7 +37,7 @@ function inverseKinematic() {
             this.b.y = this.a.y + dy
         }
 
-        paingLine () {
+        paingLine() {
             this.ctx.beginPath()
             this.ctx.moveTo(this.a.x, this.a.y)
             this.ctx.lineTo(this.b.x, this.b.y)
@@ -46,35 +46,69 @@ function inverseKinematic() {
             this.ctx.stroke()
         }
 
+        //target X and target Y
+        follow(tx, ty) {
+            let dir = {
+                x: tx - this.a.x,
+                y: ty - this.a.y
+            }
+
+            if(mouseX && mouseY)
+            this.angle = Math.atan2(ty - this.a.y, tx - this.a.x)
+        }
         update() {
             this.calculateB()
         }
 
-        show(){
-            this.ctx.fillStyle = '#515151'
-            this.ctx.fillRect(0, 0, canvasWidth, canvasHeight)
-            this.paingLine()
+        paintHelp() {
+            //letras
+            this.ctx.font = "16px Arial";
+            ctx.fillStyle = "#ffffff";
+            this.ctx.fillText("A", this.a.x - 8, this.a.y - 8);
+            this.ctx.fillText("B", this.b.x - 8, this.b.y - 8);
+            this.ctx.fillText(`(${mouseX},${mouseY})`, mouseX, mouseY)
 
+
+            this.ctx.beginPath()
+            this.ctx.moveTo(this.a.x, this.a.y)
+            this.ctx.lineTo(mouseX, mouseY)
+            this.ctx.lineWidth = 1
+            ctx.strokeStyle = "#FFFFFF";
+            this.ctx.stroke()
+            this.ctx.beginPath()
+            this.ctx.moveTo(this.b.x, this.b.y)
+            this.ctx.lineTo(mouseX, mouseY)
+
+            this.ctx.lineWidth = 1
+            ctx.strokeStyle = "#FFFFFF";
+            this.ctx.stroke()
+        }
+        show() {
+            this.ctx.fillRect(0, 0, 500, 500)
+            this.paingLine()
+            this.paintHelp()
         }
     }
 
-    let seg = new Segment({x: 200, y: 300, len: 100, angle: 0, ctx})
+    let seg = new Segment({ x: 200, y: 300, len: 100, angle: 0, ctx })
 
     //general update.
     const update = () => {
-        seg.show()
+        ctx.fillStyle = '#515151'
+        // seg.follow(mouseX, mouseY)
         seg.update()
+        seg.show()
         setTimeout(update, 16);
     };
 
     //canvas size update.
-    function resize() {
-        canvas.setAttribute('width', window.innerWidth);
-        canvas.setAttribute('height', window.innerHeight);
+    // function resize() {
+    //     canvas.setAttribute('width', window.innerWidth);
+    //     canvas.setAttribute('height', window.innerHeight);
 
-        canvasHeight = window.innerHeight
-        canvasWidth = window.innerWidth
-    }
+    //     canvasHeight = window.innerHeight
+    //     canvasWidth = window.innerWidth
+    // }
 
     function debounce(func, time = 300) {
         var timer;
@@ -85,9 +119,13 @@ function inverseKinematic() {
     }
 
     (function () {
-        window.onresize = debounce(resize);
-        resize();
-
+        // window.onresize = debounce(resize);
+        // resize();
+        canvas.addEventListener('mousemove', (event) => {
+            console.log(event)
+            mouseX = event.offsetX
+            mouseY = event.offsetY
+        });
         // for (let i = 0; i < dropsAmount; i++) {
         //     drops.push(new Drop());
         // }
